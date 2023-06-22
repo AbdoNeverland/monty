ed393e9c1a53ecb292cae946cc961a5d38206733
abdoneverland <ch.abdel.news@gmail.com>
2023-06-22 01:53:17 +0100

    late night

--- monty.c
+++ monty.c
@@ -1,18 +1,18 @@
 #include "monty.h"
+stack_t *Store = NULL;
 /**
  * processFile - process File
  * @filename: filename
  */
-void processFile(char *filename, const instruction_t *inst, stack_t **Store)
+void processFile(char *filename)
 {
 	FILE *f;
 	char *oneline = NULL;
 	char tokens[10][20];
 	size_t line_length = 0;
 	ssize_t line_length_readed;
-	long unsigned int i, ii, j, k, line_number = 0;
+	long unsigned int i, j, k, line_number = 0;
 
-print(inst[0].opcode);
 	f = fopen(filename, "r");
 	if (!f)
 	{
@@ -27,8 +27,8 @@ print(inst[0].opcode);
 		for (i = 0, j = 0, k = 0;; i++)
 		{
 			if (oneline[i] != ' ' && oneline[i] != '\n' &&
-			oneline[i] != '\t' && oneline[i] != '\0')
-			tokens[j][k] = oneline[i], k++;
+				oneline[i] != '\t' && oneline[i] != '\0')
+				tokens[j][k] = oneline[i], k++;
 			else
 			{
 				if (tokens[j][0] != 0 || oneline[i] == '\n' || oneline[i] == '\0')
@@ -41,12 +41,17 @@ print(inst[0].opcode);
 			if (!oneline[i])
 				break;
 		}
-		for (ii = 0; i < sizeof(inst) / sizeof(instruction_t); i++)
+		/*for (ii = 0; i < sizeof(inst) / sizeof(instruction_t); i++)
 		{
 			if (strcmp(inst[ii].opcode, tokens[0]) == 0)
 			inst[ii].f(Store,line_number);
-		}
+		}*/
 		printf("%s<->%s#\n", tokens[0], tokens[1]);
+		int n = atoi(tokens[1]);
+		if (strcmp(tokens[0], "push") == 0)
+		{
+			add2stack(&Store, n);
+		}
 	}
 	fclose(f);
 	if (oneline)
@@ -57,20 +62,20 @@ print(inst[0].opcode);
  * @argc: nb arguments
  * @argv: arguments
  * Return: int
-*/
+
 void i_push(stack_t **stack, unsigned int line_number)
 {
 printf("push founded ::::: %d %d",7, line_number);
-}
+}*/
 int main(int argc, char **argv)
 {
-	stack_t *Store = NULL;
-	instruction_t Instructions[] = {{"poooooow",i_push}};
+	/*stack_t *Store = NULL;
+	instruction_t Instructions[] = {{"poooooow",i_push}};*/
 	if (argc != 2)
 	{
 		print("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-processFile(argv[1], Instructions, &Store);
+	processFile(argv[1]);
 	exit(EXIT_SUCCESS);
 }
